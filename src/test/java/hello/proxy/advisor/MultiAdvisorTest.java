47ad9ca74a1f9b7c9e6d09955deb079237663363
jkkim74 <jack68@naver.com>
2022-02-17 00:24:45 +0900

    multiAdvisorTest add

--- src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -36,6 +36,28 @@ public class MultiAdvisorTest {
         proxy2.find();
 
     }
+
+    @Test
+    @DisplayName ("하나의 프록시 여러 어드바이저")
+    void multiAdvisorTest2(){
+        //client -> proxy -> advisor2 -> advisor1 -> target
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        // 프록시 1 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+
+        proxyFactory1.addAdvisor(advisor2);
+        proxyFactory1.addAdvisor(advisor1);
+
+        ServiceInterface proxy = (ServiceInterface)proxyFactory1.getProxy();
+
+        // 실행
+        proxy.save();
+        proxy.find();
+
+
+    }
     @Slf4j
     static class Advice1 implements MethodInterceptor{
 
